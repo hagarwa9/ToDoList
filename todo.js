d7eb2b99fd7a7bbf568e698a80a122874b65b99e
hagarwa9 <90210207+hagarwa9@users.noreply.github.com>
2022-04-26 08:41:13 -0700

    updated features

--- todo.js
+++ todo.js
@@ -1,44 +1,63 @@
 // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+var inCompleteItems = [
+  "Hit the gym",
+  "Meet George",
+  "Buy eggs",
+  "Read a book",
+  "Organize office",
+];
+var completeItems = ["Pay bills"];
+var showComplete = true;
+var showInComplete = true;
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+refreshList(showComplete, showInComplete);
 
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      var value = ev.target.innerText.replace("\n\u00D7", "");
+
+      if (ev.target.classList.contains("checked")) {
+        inCompleteItems.push(value);
+        const index = completeItems.findIndex((v) => v === value);
+        completeItems.splice(index, 1);
+      } else {
+        completeItems.push(value);
+        const index = inCompleteItems.findIndex((v) => v === value);
+        inCompleteItems.splice(index, 1);
+      }
+
+      refreshList(showComplete, showInComplete);
+    }
+  },
+  false
+);
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
+  if (inputValue === "") {
     alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
+    return;
   }
+
+  var inputValue = document.getElementById("myInput").value;
+  inCompleteItems.push(inputValue);
+
   document.getElementById("myInput").value = "";
+
+  refreshList();
+}
+
+function addElement(itemName, isComplete) {
+  var li = document.createElement("li");
+  var t = document.createTextNode(itemName);
+  li.appendChild(t);
+  document.getElementById("myUL").appendChild(li);
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -46,10 +65,59 @@ function newElement() {
   span.appendChild(txt);
   li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+  span.onclick = function () {
+    var div = this.parentElement;
+    var value = div.innerText.replace("\n\u00D7", "");
+    if (div.classList.contains("checked")) {
+      const index = completeItems.findIndex((v) => v === value);
+      completeItems.splice(index, 1);
+    } else {
+      const index = inCompleteItems.findIndex((v) => v === value);
+      inCompleteItems.splice(index, 1);
     }
+
+    refreshList();
+  };
+
+  if (isComplete) {
+    li.classList.add("checked");
   }
+}
+
+function refreshList() {
+  document.getElementById("myUL").innerHTML = "";
+  if (showInComplete) {
+    inCompleteItems.forEach((item) => {
+      addElement(item);
+    });
+  }
+
+  if (showComplete) {
+    completeItems.forEach((item) => {
+      addElement(item, true);
+    });
+  }
+}
+
+function toggleShowInComplete() {
+  showComplete = false;
+  showInComplete = true;
+  refreshList();
+}
+
+function toggleShowComplete() {
+  showComplete = true;
+  showInComplete = false;
+  refreshList();
+}
+
+function toggleShowAll() {
+  showComplete = true;
+  showInComplete = true;
+  refreshList();
+}
+
+function reverseListing() {
+  inCompleteItems.reverse();
+  refreshList();
 }
